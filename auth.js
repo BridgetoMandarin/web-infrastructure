1de28cc70c05c4a9dfb18eedcfb1ec99cb4839e6
RXCHEN1 <chenrita51@gmail.com>
2025-07-11 11:25:03 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,40 +1,24 @@
-// Firebase config 
-const firebaseConfig = {
-  apiKey: "AIzaSyB21zrpHKPYKcOPK-YhAm_afH9Cp37zUxY",
-  authDomain: "b2m-firebase.firebaseapp.com",
-  projectId: "b2m-firebase",
-  storageBucket: "b2m-firebase.firebasestorage.app",
-  messagingSenderId: "287571879958",
-  appId: "1:287571879958:web:dee6b7ce277e90906d6d37",
-  measurementId: "G-XK06N5MYH5"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-// Initialize FirebaseUI
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-// UI config
-const uiConfig = {
-  signInSuccessUrl: "/", // redirect after login
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
-  signInFlow: "popup"
-};
-
-// Wait for page to load before starting UI
-window.addEventListener("load", () => {
-  const container = document.getElementById("firebaseui-auth-container");
-  if (container) {
-    ui.start("#firebaseui-auth-container", uiConfig);
-  } else {
-    console.error("FirebaseUI container not found.");
-  }
-});
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyB21zrpHKPYKcOPK-YhAm_afH9Cp37zUxY",
+    authDomain: "b2m-firebase.firebaseapp.com",
+    projectId: "b2m-firebase",
+    storageBucket: "b2m-firebase.firebasestorage.app",
+    messagingSenderId: "287571879958",
+    appId: "1:287571879958:web:dee6b7ce277e90906d6d37",
+    measurementId: "G-XK06N5MYH5"
+  };
 
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
