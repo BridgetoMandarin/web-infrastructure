edaf0f3cc42399efab2816bf30eb27fa853c8efe
RXCHEN1 <chenrita51@gmail.com>
2025-07-15 12:56:29 -0400

    auth update

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 <script type="module">
-  // Firebase imports (v9+ modular SDK)
+  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
   import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -15,7 +17,8 @@
     serverTimestamp,
   } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
-  // Firebase config
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyB21zrpHKPYKcOPK-YhAm_afH9Cp37zUxY",
     authDomain: "b2m-firebase.firebaseapp.com",
